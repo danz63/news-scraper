510de7ec6d6d188c36a2b5d5e8e09d2ab14982b8
danz63 <hamdan.miftah63@gmail.com>
2021-03-16 16:10:59 +0700

    16 03 2021 16:10

--- index.php
+++ index.php
@@ -6,18 +6,3 @@ header("Location:public/");
 // $url[1] = nama_controller
 // $url[2] = nama_function
 // $url[3] = parameter
-
-if (file_exists('../app/controllers/' . $url[0] . '/' . $url[1] . '.php')) {
-    $this->controller = $url[1];
-    unset($url[0]);
-    unset($url[1]);
-}
-require_once '../app/controllers/' . $url[0] . '/' . $this->controller . '.php';
-$this->controller = new $this->controller;
-
-if (isset($url[2])) {
-    if (method_exists($this->controller, $url[2])) {
-        $this->method = $url[2];
-        unset($url[2]);
-    }
-}
